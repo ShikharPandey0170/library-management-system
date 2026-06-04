b3d67a851955584e70b5708264ed19c8e334842f
Shikhar Pandey <shikharpandey0170@gmail.com>
2026-06-04 21:43:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #main.py
 from database import setup_database
-setup_database()
 from book_functions import add_book, view_books, search_books, update_book, delete_book
 from member_functions import add_member, view_members, search_members, update_member, delete_member
 from borrow_functions import borrow_book, return_book, view_borrow_records
+
+setup_database()
 
 while True:
     print("\n\tLibrary Management System")
